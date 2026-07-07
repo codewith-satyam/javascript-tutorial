6a6910b51b58e5b807840784a27ce7beeffac117
codewith-satyam <satyam6453@gmail.com>
2026-07-07 15:19:23 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser 
+
+console.log(3+
+    3)  // code readability should be high
+
+console.log("JavaScript");
+
+let name ="satyam"  //string
+let age = 18  // number
+let isLoggedIn = false
+let state;
+ 
+// number => 2 to power 53
+// bigint
+// string => " " prefer
+// boolean => true/false
+// null standalone value
+// undefined => 
+// symbol unique
+
+
+// object 
+
+console.log(typeof age);
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined output aata hai 
+
