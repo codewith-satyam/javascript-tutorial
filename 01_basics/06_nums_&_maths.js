609ec376af068ac7124da59922f2dacc22d0bd5f
codewith-satyam <satyam6453@gmail.com>
2026-07-18 08:59:57 +0530

    Number and Math function Done

---
+++ 01_basics/06_nums_&_maths.js
@@ -0,0 +1,57 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(105.206)
+// console.log(balance);
+
+// console.log(balance.toString().length); //7
+// console.log(balance.toFixed(2)); //105.20
+
+const otherNumber = 23.916
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString()); // 1,00,000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ********* MATHS *****************
+
+console.log(Math);
+console.log(Math.abs(4)); //4
+console.log(Math.round(4.6));  //5
+console.log(Math.ceil(5.6)); //6
+console.log(Math.floor(9.2)); //9
+console.log(Math.min(9,3,4,8,26)); //3
+console.log(Math.max(9,3,4,8,26)); //26
+
+console.log(Math.random());
+console.log((Math.random()*10)+ 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
